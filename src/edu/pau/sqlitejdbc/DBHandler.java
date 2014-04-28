25f733bad1023fa3370ed8d92b700fd80d78c9f6
sevketcakir <cakiaccounts@gmail.com>
2014-04-28 16:03:59 +0300

    Latest updates -DBHandler class added -Two different connection type implemented in main method: using DBHanler class and pure Connection class -New entries added in database table

---
+++ src/edu/pau/sqlitejdbc/DBHandler.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pau.sqlitejdbc;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Şevket Umut ÇAKIR
+ */
+public class DBHandler {
+
+    private String connectionString;
+    private Connection connection;
+    private ResultSet rs;
+
+    public DBHandler() {
+        connectionString = "";
+    }
+
+    public DBHandler(String connectionString) throws SQLException {
+        this.connectionString = connectionString;
+        this.connect();
+    }
+
+    /**
+     * Sets the connection string
+     *
+     * @param connectionString the connectionString to set
+     */
+    public void setConnectionString(String connectionString) {
+        this.connectionString = connectionString;
+    }
+
+    /**
+     * Connects the database specified in connectionString
+     * @throws SQLException 
+     */
+    public final void connect() throws SQLException {
+        connection = DriverManager.getConnection(connectionString);
+    }
+
+    /**
+     * Closes database connection
+     * @throws SQLException 
+     */
+    public void close() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    /**
+     * Executes a SELECT query
+     * @param query SQL SELECT statement to query
+     * @return ResultSet object containing the result set of query
+     * @throws SQLException 
+     */
+    public ResultSet executeQuery(String query) throws SQLException {
+        if (connection != null) {
+            Statement stmt = connection.createStatement();
+            rs = stmt.executeQuery(query);
+        }
+        return rs;
+    }
+
+    /**
+     * Execute a DELETE,INSERT or an UPDATE query
+     * @param sql SQL Statement to execute
+     * @throws SQLException 
+     */
+    public void executeNonQuery(String sql) throws SQLException {
+        if (connection != null) {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(sql);
+        }
+    }
+
+    /**
+     * Prints the Resultsel to standart output
+     */
+    public void printResultSet() {
+        if (rs != null) {
+            try {
+                ResultSetMetaData md = rs.getMetaData();
+                for (int i = 1; i <= md.getColumnCount(); i++) {
+                    System.out.printf("%-35s\t", md.getColumnName(i));
+                }
+                System.out.println();
+                for (int i = 1; i <= md.getColumnCount(); i++) {
+                    System.out.printf("%-35s\t", "------------------------");
+                }
+                System.out.println();
+                while (rs.next()) {
+                    for (int i = 1; i <= md.getColumnCount(); i++) {
+                        System.out.printf("%-35s\t", rs.getObject(i));
+                    }
+                    System.out.println();
+                }
+            } catch (SQLException e) {
+                System.out.println("SQLException occured\nError message:" + e.getMessage());
+            }
+
+        }
+    }
+}
--- src/edu/pau/sqlitejdbc/SQLiteJDBC.java
+++ src/edu/pau/sqlitejdbc/SQLiteJDBC.java
@@ -2,7 +2,6 @@ package edu.pau.sqlitejdbc;
 
 import java.sql.*;
 
-
 /**
  *
  * @author Şevket Umut ÇAKIR
@@ -13,18 +12,52 @@ public class SQLiteJDBC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
+        DBHandler dbh = null;//DBHandler class to help database operations
+        Connection connection = null;//Pure Connection class
+
         try {
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+            /*
+             * Connect and use DBHandler class methods
+             * Print all data in the table
+             */
+            String sql = "SELECT * FROM Employee";
+            String connectionString = "jdbc:sqlite:sample.db";
+
+            dbh = new DBHandler(connectionString);
+            dbh.executeQuery(sql);
+            dbh.printResultSet();
+            //Randomly update department and title of the dummy entry
+            dbh.executeNonQuery("UPDATE Employee SET Department='" + Math.random() + "',Title='" + Math.random() + "' WHERE Id=21");
+
+            /*
+             * Use Connection, Statement, ResultSet classes
+             * Write names of employees
+             */
+            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
             Statement stmt = connection.createStatement();
-            ResultSet rs=stmt.executeQuery("SELECT * FROM Employee");
-            while(rs.next())//iterate through rows
-            {
-                System.out.printf("%-15s\t%-15s\t%-15s\n",rs.getObject("EmployeeId"),rs.getObject("EmployeeName"),rs.getObject("EmployeeLastName"));
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Employee");
+            while (rs.next()) {
+                System.out.println(rs.getObject("FirstName"));
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (dbh != null) {
+                    dbh.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("Can't close DBHandler object\nReason:" + e.getMessage());
+            }
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("Can't close Connection object\nReason:" + e.getMessage());
+            }
         }
     }
-    
+
 }
